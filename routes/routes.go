1eb2b35759c71d6e939602d5752cbba2d813e53c
LeGion <glissovskiy@gmail.com>
2022-08-06 13:16:37 -0700

    added util

--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"golab.info/go-admin/controllers"
+	"golab.info/go-admin/middlewares"
 )
 
 func Setup(app *fiber.App) {
@@ -11,6 +12,9 @@ func Setup(app *fiber.App) {
 	app.Get("/other", controllers.Other)
 	app.Post("api/register", controllers.Register)
 	app.Post("api/login", controllers.Login)
+
+	app.Use(middlewares.IsAuthenticated)
+
 	app.Get("api/user", controllers.User)
 	app.Post("api/logout", controllers.Logout)
 
