9cd6692ba972bbba37235ed746a6108e940234be
LeGion <glissovskiy@gmail.com>
2022-08-21 09:38:30 -0700

    added metadata last_page

--- controllers/userConroller.go
+++ controllers/userConroller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"math"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
@@ -12,15 +13,20 @@ func AllUsers(c *fiber.Ctx) error {
 	page, _ := strconv.Atoi(c.Query("page", "1"))
 	limit := 5
 	offset := (page - 1) * limit
+	var total int64
 	var users []models.User
 
 	database.DB.Preload("Role").Offset(offset).Limit(limit).Find(&users)
+
+	database.DB.Model(&models.User{}).Count(&total)
 
 	//return c.JSON(users)
 	return c.JSON(fiber.Map{
 		"data": users,
 		"meta": fiber.Map{
-			"page": page,
+			"total":     total,
+			"page":      page,
+			"last_page": math.Ceil(float64(int(total) / limit)),
 		},
 	})
 }
