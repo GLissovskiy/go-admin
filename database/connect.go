4320a939e24d943e4010eacd702d7f2fae7bc07b
LeGion <glissovskiy@gmail.com>
2022-08-27 10:05:25 -0700

    added Product model, controller

--- database/connect.go
+++ database/connect.go
@@ -18,6 +18,6 @@ func Connect() {
 
 	DB = db
 
-	db.AutoMigrate(&models.User{}, &models.Role{}, &models.Permission{})
+	db.AutoMigrate(&models.User{}, &models.Role{}, &models.Permission{}, &models.Product{})
 
 }
