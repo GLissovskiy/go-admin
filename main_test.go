d82770b7c8b141874c57cbc4dcfc872f950b9b33
LeGion <glissovskiy@gmail.com>
2022-09-19 16:35:38 -0700

    added Total + Name

---
+++ main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestParallelOne(t *testing.T) {
+	t.Parallel()
+	time.Sleep(3 * time.Second)
+
+}
+
+func TestParallelTwo(t *testing.T) {
+	t.Parallel()
+	time.Sleep(3 * time.Second)
+}
+
+func TestParallelThree(t *testing.T) {
+	t.Parallel()
+	time.Sleep(3 * time.Second)
+}
